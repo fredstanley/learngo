3c10d5a39f840f3dc2d0339270de0cea8d3c3d92
fredstanley <fredphilip@gmail.com>
2017-06-30 17:38:47 -0700

    Update test.go

--- test.go
+++ test.go
@@ -5,10 +5,13 @@ import "fmt"
 func main() {
 	fmt.Printf("hello, world\n")
 	fmt.Printf("test")
-	fmt.Printf("test...")
+	fmt.Printf("chanted");
     fmt.Printf("Test git commit...\n");
 	
 	
 	fmt.Printf("test pull request \n");
+	
+	
+	
 	
 }
